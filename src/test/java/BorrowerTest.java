3c2fb90bf1734ca6cc5a9cde3b8ff7c4a3fceda3
DanielLyleWilliamWarren <daniel.warren.deen@gmail.com>
2018-07-18 14:26:38 +0100

    possibly solved crisis

--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -1,2 +1,20 @@
+import models.Borrower;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BorrowerTest {
+
+    Borrower borrower;
+
+    @Before
+    public void setup(){
+        borrower = new Borrower("Dave");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Dave", borrower.getName());
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,3 +1,35 @@
+import models.Book;
+import models.Borrower;
+import models.Library;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    Library library;
+    ArrayList<Book> books;
+    ArrayList<Borrower> borrowers;
+
+    @Before
+    public void setup(){
+        books = new ArrayList<Book>();
+        borrowers = new ArrayList<Borrower>();
+        library = new Library(books, borrowers);
+    }
+
+    @Test
+    public void hasBooks(){
+        assertEquals(books, library.getBooks());
+    }
+
+    @Test
+    public void hasBorrowers(){
+        assertEquals(borrowers, library.getBorrowers());
+    }
+
 
 }
