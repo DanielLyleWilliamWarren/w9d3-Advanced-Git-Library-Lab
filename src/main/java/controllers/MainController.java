bd9c1ede84ba1fe5bfca8a876328f8fa38eb09e9
Wil Cornish <wilcornish@icloud.com>
2018-07-18 14:19:55 +0100

    adds borrower controller

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -13,6 +13,7 @@ public class MainController {
     public static void main(String[] args) {
 
         BookController bookController = new BookController();
+        BorrowerController borrowerController = new BorrowerController();
 
 
         VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
