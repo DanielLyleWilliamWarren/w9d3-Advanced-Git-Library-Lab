48f767ae23d1d52d4c2750f7d42cb557477a399c
DanielLyleWilliamWarren <daniel.warren.deen@gmail.com>
2018-07-18 12:06:14 +0100

    almost setup models

--- src/main/java/models/Book.java
+++ src/main/java/models/Book.java
@@ -1,4 +1,63 @@
 package models;
 
 public class Book {
+
+    private int id;
+    private String title;
+    private String author;
+    private boolean onLoan;
+    private Borrower borrower;
+    public Book(){}
+
+    public Book(String title, String author, boolean onLoan) {
+        this.title = title;
+        this.author = author;
+        this.onLoan = onLoan;
+    }
+
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "title")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Column(name = "author")
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    @Column(name = "onLoan")
+    public boolean isOnLoan() {
+        return onLoan;
+    }
+
+    public void setOnLoan(boolean onLoan) {
+        this.onLoan = onLoan;
+    }
+
+    public Borrower getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(Borrower borrower) {
+        this.borrower = borrower;
+    }
+
 }
--- src/main/java/models/Borrower.java
+++ src/main/java/models/Borrower.java
@@ -1,4 +1,43 @@
 package models;
 
+import java.util.Set;
+
 public class Borrower {
+
+    private int id;
+    private String name;
+    private Set<Book> books;
+
+    public Borrower(){}
+
+    public Borrower(String name){
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
 }
