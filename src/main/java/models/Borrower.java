dbd8558c0ce30be49f3c41286f8c2288f3d9c47b
Wil Cornish <wilcornish@icloud.com>
2018-07-18 12:01:17 +0100

    adds setup for borrower class

--- src/main/java/models/Borrower.java
+++ src/main/java/models/Borrower.java
@@ -1,4 +1,37 @@
 package models;
 
 public class Borrower {
+    private int id;
+    private String name;
+    private List<books> itemsBorrowed;
+
+    public borrower(String name){
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<books> getItemsBorrowed() {
+        return itemsBorrowed;
+    }
+
+    public void setItemsBorrowed(List<books> itemsBorrowed) {
+        this.itemsBorrowed = itemsBorrowed;
+    }
 }
